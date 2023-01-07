037a1810526129d0d8da2435fc46e39800ea77ba
Dahee Park <byeori0306@gmail.com>
2023-01-08 01:01:03 +0900

    feat: global CSS 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,15 @@ export default function App() {
     const [counter, setCounter] = useState(0);
 
     return (
-        <main>
-            <button onClick={() => setCounter((prev) => prev + 1)}>
-                카운터 증가!!
+        <main css={mainStyle}>
+            <button
+                css={buttonStyle}
+                onClick={() => setCounter((prev) => prev + 1)}
+            >
+                HMR test button
             </button>
             <div css={wrapperStyle}>
-                <h1>
+                <h1 css={titleStyle}>
                     {`My React template - ${process.env.NODE_ENV}
                     ${process.env.name} / counter: ${counter}`}
                 </h1>
@@ -23,14 +26,36 @@ export default function App() {
     );
 }
 
+const mainStyle = css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 3.2rem 0;
+`;
+
+const buttonStyle = css`
+    border: 0.2rem solid #5e4324;
+    border-radius: 1.6rem;
+    font-size: 1.6rem;
+    font-weight: 700;
+    line-height: 1.6rem;
+    padding: 1rem;
+`;
+
 const wrapperStyle = css`
     display: flex;
-    gap: 10px;
     align-items: center;
 `;
 
+const titleStyle = css`
+    font-size: 3.2rem;
+    font-weight: 700;
+    color: #5e4324;
+    margin: 1rem 0.8rem;
+`;
+
 const iconStyle = css`
-    width: 40px;
-    height: 38px;
-    fill: brown;
+    width: 4rem;
+    height: 3.8rem;
+    fill: #5e4324;
 `;
